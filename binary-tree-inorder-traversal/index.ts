4db31cc0639d6227ab6babb47cc68cd843515807
tohsaka888 <156132264@qq.com>
2022-11-07 16:30:25 +0800

    🦄 refactor: update example

--- binary-tree-inorder-traversal/index.ts
+++ binary-tree-inorder-traversal/index.ts
@@ -1,6 +1,9 @@
+import { arrayToTree } from "../utils/array-to-tree.ts";
 import { TreeNode } from "/type.ts";
 
-type InorderTraversalFunc<T = number> = (root: TreeNode<T> | null) => T[];
+type InorderTraversalFunc<T = number | null> = (
+  root: TreeNode<T> | null
+) => T[];
 
 /**
  * 二叉树的中序遍历
@@ -10,17 +13,22 @@ type InorderTraversalFunc<T = number> = (root: TreeNode<T> | null) => T[];
  */
 const inorderTraversal: InorderTraversalFunc = (root) => {
   const res: number[] = [];
-  const exploredTree: TreeNode<number>[] = [];
+  const exploredTree: TreeNode<number | null>[] = [];
 
   while (root || exploredTree.length) {
-    while (root) {
+    if (root) {
       exploredTree.push(root);
       root = root.left;
+    } else {
+      const fNode = exploredTree.pop();
+      res.push(fNode!.val!);
+      root = fNode!.right;
     }
-    const fNode = exploredTree.pop();
-    res.push(fNode!.val);
-    root = fNode!.right;
   }
 
   return res;
 };
+
+const result = inorderTraversal(arrayToTree([1, null, 2, 3]));
+
+console.log(result);
