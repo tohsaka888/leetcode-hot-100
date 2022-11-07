91a5f5596f5576a1c505243610cb58982f2bd3be
tohsaka888 <156132264@qq.com>
2022-11-07 16:30:40 +0800

    ✨ feat(utils): array to tree

---
+++ utils/array-to-tree.ts
@@ -0,0 +1,37 @@
+import { TreeNode } from "../type.ts";
+
+function createTree<T>(root: TreeNode<T>, leftVal: T, rightVal: T) {
+  if (root) {
+    if (leftVal) {
+      root.left = {
+        val: leftVal,
+        left: null,
+        right: null,
+      };
+    }
+    if (rightVal) {
+      root.right = {
+        val: rightVal,
+        left: null,
+        right: null,
+      };
+    }
+  }
+}
+
+function arrayToTree<T>(arr: T[]) {
+  let i = 0;
+  const tree: TreeNode<T> = { val: arr[0], left: null, right: null };
+  const queue = [tree];
+  while (i < arr.length) {
+    const node = queue.shift();
+    if (node) {
+      createTree(node, arr[i + 1], arr[i + 2]);
+      i = i + 2;
+      queue.push(node.left!, node.right!);
+    }
+  }
+  return tree;
+}
+
+export { arrayToTree };
