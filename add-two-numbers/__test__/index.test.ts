1097162392030f1827dee370c569d184da286a2b
tohsaka888 <156132264@qq.com>
2022-11-16 11:50:13 +0800

    🧪 test: 两数相加

--- add-two-numbers/__test__/index.test.ts
+++ add-two-numbers/__test__/index.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.164.0/testing/asserts.ts";
 import { arrayToList } from "/utils/array-to-list.ts";
 import addTwoNumbers from "../index.ts";
 
-Deno.test("两数之和", () => {
+Deno.test("两数相加", () => {
   assertEquals(addTwoNumbers(
     arrayToList([9, 9, 9, 9, 9, 9]),
     arrayToList([9, 9, 9])
